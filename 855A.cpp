25cc9c2e75cf0a0c81723fb5dfea18db2bc3949c
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2022-09-10 01:18:12 +0600

    A. Tom Riddle's Diary ( Codeforces )

---
+++ 855A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define yes printf("YES\n")
+#define no printf("NO\n")
+#define op()                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+int main()
+{
+    op();
+
+    int n;
+    cin >> n;
+    map<string, bool> ch;
+
+    while (n--)
+    {
+        string s;
+        cin >> s;
+        if (ch[s] == 1)
+            yes;
+        else
+            no;
+
+        ch[s] = 1;
+    }
+    return 0;
+}
