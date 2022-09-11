d9763ba535604e56e0c117326028f23e73f56837
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2022-09-12 02:24:55 +0600

    A. I Wanna Be the Guy
    https://codeforces.com/problemset/problem/469/A

---
+++ 469A.cpp
@@ -0,0 +1,29 @@
+// IN the name of ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    set<int> s;
+    int p;
+    cin >> p;
+    for (int i = 0; i < p; i++)
+    {
+        int a;
+        cin >> a;
+        s.insert(a);
+    }
+    cin >> p;
+    for (int i = 0; i < p; i++)
+    {
+        int a;
+        cin >> a;
+        s.insert(a);
+    }
+    if (s.size() == n)
+        cout << "I become the guy.\n";
+    else
+        cout << "Oh, my keyboard!\n";
+}
