a1a51b8a41fd6981edc8c98fbf5238ca6ad19d30
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2022-09-15 22:17:32 +0600

    Monk and Multiplication
    https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/monk-and-multiplication/

---
+++ Priority_queue.cpp
@@ -0,0 +1,35 @@
+// in the name of ALLAH
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mx = 1e5 + 123;
+int a[mx];
+
+int main()
+{
+    priority_queue<int> q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < n; i++)
+    {
+        q.push(a[i]);
+        if (q.size() >= 3)
+        {
+            int a1 = q.top();
+            q.pop();
+            int a2 = q.top();
+            q.pop();
+            int a3 = q.top();
+            q.pop();
+            cout << 1ll * a1 * a2 * a3 << "\n";
+            q.push(a1);
+            q.push(a2);
+            q.push(a3);
+        }
+        else
+            cout << -1 << "\n";
+    }
+}
