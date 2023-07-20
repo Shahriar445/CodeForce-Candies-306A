364d1867d649f792d013a33223165b25b33f8e45
Shahriar Haque <91280914+Shahriar445@users.noreply.github.com>
2023-07-21 01:29:53 +0600

    A. Coins
    https://codeforces.com/contest/1814/problem/A

---
+++ test.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int 
+#define F first
+#define S second 
+#define pb push_back
+#define endl "\n"
+int BinarrySearch(vector <int> & arr, int x){
+	int low=0, hi=arr.size()-1;
+
+	while (hi-low > 1){
+		int mid =(hi+low)/2;
+
+		if (arr[mid]<x){
+			low=mid+1;
+		}
+		else {
+			hi=mid;
+		}
+	}
+
+	if (arr[low]==x){
+	    return low;
+	}
+	else if (arr[hi]==x){
+		return hi;
+	}
+	else {
+		return -1;
+	}
+}	
+
+// Write your logic here 
+
+void solve()
+{
+	int n,k;
+	cin>>n>>k;
+	if (n%2==0)cout<<"YES"<<endl;
+	else if (k%2!=0)
+		{
+			cout<<"yes"<<endl;}
+	else 
+	cout<<"no"<<endl;
+	
+}
+
+int32_t main(){
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+	int t=1;
+	cin >> t;
+	while (t--) solve();
+
+
+
+	//BinarrySearch(arr,x)<<endl;
+
+	return 0;
+
+}
